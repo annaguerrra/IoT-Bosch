84441d770f36269986904c87abed0f13a9675f9e
annaguerrra <annaguerracastro@gmail.com>
2026-01-23 14:29:15 -0300

    mqttString

---
+++ mqttString/env.h
@@ -0,0 +1,14 @@
+#ifndef ENV_H
+#define ENV_H
+#define WIFI_SSID "BTIA_SON5CT"
+#define WIFI_PASS "IUVr4qSh8kQe"
+#define LED_PIN 22                                                             //led que já vem soldado na esp32 
+
+const char* mqtt_server    = "broker.hivemq.com";
+const char* mqtt_client_id = "esp32_alunx";                                    // <---- Coloca o seu nomezinho aqui
+const char* topico_comando = "bosch/ets/dta/luz";
+const char* topico_status  = "bosch/ets/dta/status";
+const char* topico_texto   = "bosch/ets/dta/texto";
+const int   mqtt_port      =  1883;
+
+#endif
