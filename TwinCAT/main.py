39273571bc73cba542fb2031839d0bdf097ef809
annaguerrra <annaguerracastro@gmail.com>
2026-02-11 13:54:23 -0300

    Iterando Malo

--- TwinCAT/main.py
+++ TwinCAT/main.py
@@ -1,27 +1,25 @@
 import pyads
-import math
+
 
 # criando um objeto de conexão com PLC
 
 # AMS Net ID -> id de qual PLC eu quero acessar
-lays = "10.234.199.179.1.1"
+malo = "10.234.198.62.1.1"
+ipMalo = "10.234.199.183"
 
 # ip de quem está enviando
-anna = "10.234.195.54.1.1"
+anna = "10.234.195.79.1.1"
 
 # porta do serviço do PLC -> canal de comunicação
 porta = pyads.PORT_TC3PLC1
-plc = pyads.Connection(lays, porta, )
-# Começando a conexão
+plc = pyads.Connection(malo, porta, ipMalo)
+
+# iterando na váriavel 
 plc.open()
+# y = plc.read_by_name("GVL.x")
 
-x = int(input("Digite um número: "))
+plc.write_by_name("GVL.tempo", 2)
 
-if math.isnan(x):
-    Led += x
-else:
-    print("Escolha um valor válido!")
-    
-    
-# fechando conexão
 plc.close()
+
+
