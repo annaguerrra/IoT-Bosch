ee9d0bf53b0195436d092bc4e03ffab6a009a6fb
devGuerra <annaguerracastro@gmail.com>
2026-02-18 14:59:58 -0300

    update

--- DesafioEsteira/sketch_feb13a/sketch_feb13a.ino
+++ DesafioEsteira/sketch_feb13a/sketch_feb13a.ino
@@ -6,20 +6,25 @@ WiFiClient espClient;
 PubSubClient client(espClient);
 
 const int pinIR = 32;
-int statusIR = 0;
+int lastState = HIGH;  
 
-void WiFiEvent(WiFiEvent_t event){ 
-  switch(event){ 
-    case ARDUINO_EVENT_WIFI_STA_GOT_IP: 
-      Serial.printl("WiFi conectado!"); 
-      break; 
-    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED: 
-      Serial.println("WiFi perdido. Conectando novamente"); 
-      break; 
+void WiFiEvent(WiFiEvent_t event){
+  switch(event){
+
+    case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+      Serial.println("WiFi conectado!");
+      attemptMqttConnection();
+      break;
+
+    case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+      Serial.println("WiFi perdido!");
+      WiFi.begin(ssid, password);
+      break;
   }
 }
 
 boolean attemptMqttConnection() {
+
   String clientId = String(mqtt_client_id) + "_" + WiFi.macAddress();
 
   if (client.connect(
@@ -33,11 +38,10 @@ boolean attemptMqttConnection() {
       )) {
 
     Serial.println("MQTT conectado!");
-
     client.publish(topico_status, "ONLINE", true);
     client.subscribe(topico_comando);
-
     return true;
+
   } else {
     Serial.print("Erro MQTT: ");
     Serial.println(client.state());
@@ -50,34 +54,44 @@ void callback(char* topic, byte* payload, unsigned int length) {
 }
 
 void setup() {
+
   Serial.begin(115200);
 
   pinMode(pinIR, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  connectWiFi();
+  WiFi.onEvent(WiFiEvent);
+
+  WiFi.begin(ssid, password);
 
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
 }
 
 void loop() {
-  if (!client.connected()) {
-    connectMQTT();
-  }
+
+  if (!client.connected())
+    attemptMqttConnection();
+
   client.loop();
+
   int sensorState = digitalRead(pinIR);
 
-  if (sensorState == LOW) {
-    digitalWrite(LED_BUILTIN, HIGH);
-    client.publish(topico_status, "OBJETO_DETECTADO", true);
-    Serial.println("Objeto detectado");
-  } 
-  else {
-    digitalWrite(LED_BUILTIN, LOW);
-    client.publish(topico_status, "SEM_OBJETO", true);
+  if (sensorState != lastState) {
+
+    lastState = sensorState;
+
+    if(sensorState == LOW){
+      digitalWrite(LED_BUILTIN, HIGH);
+      client.publish(topico_status, "OBJETO_DETECTADO", true);
+      Serial.println("Objeto detectado");
+    }
+    else{
+      digitalWrite(LED_BUILTIN, LOW);
+      client.publish(topico_status, "SEM_OBJETO", true);
+      Serial.println("Sem objeto");
+    }
   }
-}
 
   delay(100);
 }
